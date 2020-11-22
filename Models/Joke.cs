1a076742d2efce57698c23ed027b386474035a48
aalshibily <aalshibaily@live.com>
2020-11-21 21:33:34 -0600

    Added random joke fetching TODO List is still WIP

---
+++ Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorStaticWebsite.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Setup { get; set; }
+        public string Punchline { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -18,7 +18,18 @@ namespace BlazorStaticWebsite
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //builder.Services.AddScoped(x => new HttpClient()
+            //{ BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddHttpClient("Default", cl =>
+            {
+                cl.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+            });
+
+            builder.Services.AddHttpClient("Joke", cl =>
+            {
+                cl.BaseAddress = new Uri("https://official-joke-api.appspot.com");
+            });
 
             //Add Blazored LocalStorage support
             //Also pass in option to prettify the JSON
