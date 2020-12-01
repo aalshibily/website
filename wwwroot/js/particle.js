2ef51a3389d8f60e49e0942dce68b66934ee3cab
aalshibily <aalshibaily@live.com>
2020-11-30 22:33:59 -0600

    Added Particle.js for style. Might switch to blazor interop later?

---
+++ wwwroot/js/particle.js
@@ -0,0 +1,9 @@
+﻿window.onload = function () {
+    console.log("Initializing Particle JS");
+    Particles.init({
+        selector: '.background',
+        color: "#00FFFF",
+        maxParticles: 100,
+        connectParticles: true
+    });
+};
